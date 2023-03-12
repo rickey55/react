952b1901e648fcfdad0564c11611de893c071ae3
rickey55 <rickeysue@gmail.com>
2023-03-12 20:08:26 +0800

    :bug: fix(1): fix
    Signed-off-by: rickey55 <rickeysue@gmail.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -91,7 +91,7 @@ const Home = () => {
               <i className="fas fa-fw fa-folder"></i>
               <span>Pages</span>
             </a>
-            <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            {/* <div id="collapsePages" className="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
               <div className="bg-white py-2 collapse-inner rounded">
                 <h6 className="collapse-header">Login Screens:</h6>
                 <a className="collapse-item" href="login.html">Login</a>
@@ -102,7 +102,7 @@ const Home = () => {
                 <a className="collapse-item" href="404.html">404 Page</a>
                 <a className="collapse-item" href="blank.html">Blank Page</a>
               </div>
-            </div>
+            </div> */}
           </li>
 
           {/* <!-- Nav Item - Charts --> */}
