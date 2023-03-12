18703a655691a5eeb5da6bb77d1956a9414c3ff5
rickey55 <rickeysue@gmail.com>
2023-03-12 21:14:26 +0800

    :sparkles: feat(2): 2
    2
    
    2
    
    Signed-off-by: rickey55 <rickeysue@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,16 @@
 import './App.css';
 import React from 'react';
-// import { useNavigate } from "react-router-dom";
 import "./css/login.css"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 
-
 function App() {
-  
   return (
     <BrowserRouter>
-      {/* <Header /> */}
-      {/* <ScrollToTop /> */}
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
