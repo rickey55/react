f7ac133a1f04c1a334d886562d460edb5756f475
rickey55 <rickeysue@gmail.com>
2023-03-12 19:56:04 +0800

    :sparkles: feat(): fix home bug
    Signed-off-by: rickey55 <rickeysue@gmail.com>

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -47,7 +47,7 @@ const Home = () => {
 
           {/* <!-- Nav Item - Pages Collapse Menu --> */}
           <li className="nav-item">
-            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+            <a className="nav-link collapsed"   data-toggle="collapse" data-target="#collapseTwo"
               aria-expanded="true" aria-controls="collapseTwo">
               <i className="fas fa-fw fa-cog"></i>
               <span>Components</span>
@@ -63,7 +63,7 @@ const Home = () => {
 
           {/* <!-- Nav Item - Utilities Collapse Menu --> */}
           <li className="nav-item">
-            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+            <a className="nav-link collapsed"   data-toggle="collapse" data-target="#collapseUtilities"
               aria-expanded="true" aria-controls="collapseUtilities">
               <i className="fas fa-fw fa-wrench"></i>
               <span>Utilities</span>
@@ -90,7 +90,7 @@ const Home = () => {
 
           {/* <!-- Nav Item - Pages Collapse Menu --> */}
           <li className="nav-item">
-            <a className="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+            <a className="nav-link collapsed"   data-toggle="collapse" data-target="#collapsePages"
               aria-expanded="true" aria-controls="collapsePages">
               <i className="fas fa-fw fa-folder"></i>
               <span>Pages</span>
@@ -175,7 +175,7 @@ const Home = () => {
 
                 {/* <!-- Nav Item - Search Dropdown (Visible Only XS) --> */}
                 <li className="nav-item dropdown no-arrow d-sm-none">
-                  <a className="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
+                  <a className="nav-link dropdown-toggle"  id="searchDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i className="fas fa-search fa-fw"></i>
                   </a>
@@ -199,7 +199,7 @@ const Home = () => {
 
                 {/* <!-- Nav Item - Alerts --> */}
                 <li className="nav-item dropdown no-arrow mx-1">
-                  <a className="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button"
+                  <a className="nav-link dropdown-toggle"  id="alertsDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i className="fas fa-bell fa-fw"></i>
                     {/* <!-- Counter - Alerts --> */}
@@ -211,7 +211,7 @@ const Home = () => {
                     <h6 className="dropdown-header">
                       Alerts Center
                     </h6>
-                    <a className="dropdown-item d-flex align-items-center" href="#">
+                    <a className="dropdown-item d-flex align-items-center" >
                       <div className="mr-3">
                         <div className="icon-circle bg-primary">
                           <i className="fas fa-file-alt text-white"></i>
@@ -222,7 +222,7 @@ const Home = () => {
                         <span className="font-weight-bold">A new monthly report is ready to download!</span>
                       </div>
                     </a>
-                    <a className="dropdown-item d-flex align-items-center" href="#">
+                    <a className="dropdown-item d-flex align-items-center"  >
                       <div className="mr-3">
                         <div className="icon-circle bg-success">
                           <i className="fas fa-donate text-white"></i>
@@ -233,7 +233,7 @@ const Home = () => {
                         $290.29 has been deposited into your account!
                       </div>
                     </a>
-                    <a className="dropdown-item d-flex align-items-center" href="#">
+                    <a className="dropdown-item d-flex align-items-center"  >
                       <div className="mr-3">
                         <div className="icon-circle bg-warning">
                           <i className="fas fa-exclamation-triangle text-white"></i>
@@ -244,13 +244,13 @@ const Home = () => {
                         Spending Alert: We've noticed unusually high spending for your account.
                       </div>
                     </a>
-                    <a className="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
+                    <a className="dropdown-item text-center small text-gray-500"  >Show All Alerts</a>
                   </div>
                 </li>
 
                 {/* <!-- Nav Item - Messages --> */}
                 <li className="nav-item dropdown no-arrow mx-1">
-                  <a className="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button"
+                  <a className="nav-link dropdown-toggle"   id="messagesDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <i className="fas fa-envelope fa-fw"></i>
                     {/* <!-- Counter - Messages --> */}
@@ -262,7 +262,7 @@ const Home = () => {
                     <h6 className="dropdown-header">
                       Message Center
                     </h6>
-                    <a className="dropdown-item d-flex align-items-center" href="#">
+                    <a className="dropdown-item d-flex align-items-center"  >
                       <div className="dropdown-list-image mr-3">
                         {/* <img className="rounded-circle" src="img/undraw_profile_1.svg"
                                   alt="..."> */}
@@ -274,7 +274,7 @@ const Home = () => {
                         <div className="small text-gray-500">Emily Fowler · 58m</div>
                       </div>
                     </a>
-                    <a className="dropdown-item d-flex align-items-center" href="#">
+                    <a className="dropdown-item d-flex align-items-center"  >
                       <div className="dropdown-list-image mr-3">
                         {/* <img className="rounded-circle" src="img/undraw_profile_2.svg"
                                   alt="..."/> */}
@@ -286,7 +286,7 @@ const Home = () => {
                         <div className="small text-gray-500">Jae Chun · 1d</div>
                       </div>
                     </a>
-                    <a className="dropdown-item d-flex align-items-center" href="#">
+                    <a className="dropdown-item d-flex align-items-center"  >
                       <div className="dropdown-list-image mr-3">
                         {/* <img className="rounded-circle" src="img/undraw_profile_3.svg"
                                   alt="..."> */}
@@ -298,7 +298,7 @@ const Home = () => {
                         <div className="small text-gray-500">Morgan Alvarez · 2d</div>
                       </div>
                     </a>
-                    <a className="dropdown-item d-flex align-items-center" href="#">
+                    <a className="dropdown-item d-flex align-items-center"  >
                       <div className="dropdown-list-image mr-3">
                         {/* <img className="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
                                   alt="..."> */}
@@ -310,7 +310,7 @@ const Home = () => {
                         <div className="small text-gray-500">Chicken the Dog · 2w</div>
                       </div>
                     </a>
-                    <a className="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
+                    <a className="dropdown-item text-center small text-gray-500"  >Read More Messages</a>
                   </div>
                 </li>
 
@@ -318,7 +318,7 @@ const Home = () => {
 
                 {/* <!-- Nav Item - User Information --> */}
                 <li className="nav-item dropdown no-arrow">
-                  <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
+                  <a className="nav-link dropdown-toggle" id="userDropdown" role="button"
                     data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     <span className="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
                     {/* <img className="img-profile rounded-circle"
@@ -327,20 +327,20 @@ const Home = () => {
                   {/* <!-- Dropdown - User Information --> */}
                   <div className="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                     aria-labelledby="userDropdown">
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item"  >
                       <i className="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
                       Profile
                     </a>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item"  >
                       <i className="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
                       Settings
                     </a>
-                    <a className="dropdown-item" href="#">
+                    <a className="dropdown-item" >
                       <i className="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
                       Activity Log
                     </a>
                     <div className="dropdown-divider"></div>
-                    <a className="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                    <a className="dropdown-item"  data-toggle="modal" data-target="#logoutModal">
                       <i className="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                       Logout
                     </a>
@@ -358,7 +358,7 @@ const Home = () => {
               {/* <!-- Page Heading --> */}
               <div className="d-sm-flex align-items-center justify-content-between mb-4">
                 <h1 className="h3 mb-0 text-gray-800">Dashboard</h1>
-                <a href="#" className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                <a  className="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                   className="fas fa-download fa-sm text-white-50"></i> Generate Report</a>
               </div>
 
