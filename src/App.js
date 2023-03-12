13b0879b9ae7721f80bc0926184a3c521e11fdb3
rickey55 <rickeysue@gmail.com>
2023-03-12 21:16:55 +0800

    :sparkles: feat(1): 1
    1
    
    1
    
    Signed-off-by: rickey55 <rickeysue@gmail.com>

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/Home" element={<Home />} />
       </Routes>
     </BrowserRouter>
